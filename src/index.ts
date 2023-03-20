f05c8085e6e5a187103548981e879236f6e1d989
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-03-20 20:02:38 +0100

    Scroll class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("");
+import { ScrollIntoView } from "./types";
+
+class Scroll {
+  public static enable(selector = "body"): void {
+    const element = document.querySelector<HTMLElement>(selector);
+
+    if (!element) return;
+
+    element.style.overflow = "";
+  }
+
+  public static disable(selector = "body"): void {
+    const element = document.querySelector<HTMLElement>(selector);
+
+    if (!element) return;
+
+    element.style.overflow = "hidden";
+  }
+
+  public static intoView({
+    target,
+    behavior = "smooth",
+    block = "start",
+    inline = "nearest",
+  }: ScrollIntoView): void {
+    document.querySelector(target)?.scrollIntoView({
+      behavior: behavior,
+      block: block,
+      inline: inline,
+    });
+  }
+}
+
+export default Scroll;
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type ScrollIntoView = {
+  target: string;
+} & ScrollIntoViewOptions;
