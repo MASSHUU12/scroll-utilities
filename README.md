a5a50976aeae2e258856cd3088e7b6c87f0f39c2
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-03-20 21:58:31 +0100

    Documentation update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TS Scroll
+
+Library that provides utilities for scrolling and managing scroll behavior.
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { ScrollIntoView } from "./types";
 
+/**
+ * A static class that provides utilities for scrolling and managing scroll behavior.
+ */
 class Scroll {
-  public static enable(selector = "body"): void {
+  /**
+   * Enables scrolling on an HTML element by setting its overflow style to an empty string.
+   *
+   * @static
+   * @param {string} [selector="body"] - A CSS selector for the HTML element to enable scrolling on. Defaults to "body".
+   * @return {*}  {void}
+   * @memberof Scroll
+   */
+  public static enable(selector: string = "body"): void {
     const element = document.querySelector<HTMLElement>(selector);
 
     if (!element) return;
@@ -9,6 +20,14 @@ class Scroll {
     element.style.overflow = "";
   }
 
+  /**
+   * Disables scrolling on an HTML element by setting its overflow style to "hidden".
+   *
+   * @static
+   * @param {string} [selector="body"] - A CSS selector for the HTML element to disable scrolling on. Defaults to "body".
+   * @return {*}  {void}
+   * @memberof Scroll
+   */
   public static disable(selector = "body"): void {
     const element = document.querySelector<HTMLElement>(selector);
 
@@ -17,16 +36,27 @@ class Scroll {
     element.style.overflow = "hidden";
   }
 
+  /**
+   * Scrolls an HTML element into view using the specified options.
+   *
+   * @static
+   * @param {Readonly<ScrollIntoView>} options - An options object that specifies the target element to scroll to and the scrolling behavior to use.
+   * @param options.target - A CSS selector or DOM element to scroll into view.
+   * @param options.behavior - The scrolling behavior to use. Defaults to "smooth".
+   * @param options.block - The vertical position to align the target element within the viewport. Defaults to "start".
+   * @param options.inline - The horizontal position to align the target element within the viewport. Defaults to "nearest".
+   * @memberof Scroll
+   */
   public static intoView({
     target,
     behavior = "smooth",
     block = "start",
     inline = "nearest",
-  }: ScrollIntoView): void {
+  }: Readonly<ScrollIntoView>): void {
     document.querySelector(target)?.scrollIntoView({
-      behavior: behavior,
-      block: block,
-      inline: inline,
+      behavior,
+      block,
+      inline,
     });
   }
 }
